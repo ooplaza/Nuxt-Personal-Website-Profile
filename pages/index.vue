2fd64393203f8297089a28299cfc80ed7cf14389
0R1-qt <orly.plaza@hcdc.edu.ph>
2022-12-24 02:31:45 +0800

    main:updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
   <v-app>
-    <v-main>
-      <!-- Header -->
-      <v-card class="text-center mt-5 mb-5" flat tile width="100%">
-        <!-- Card for Icons -->
-        <v-row>
-          <v-card-text link>
-            <v-btn plain>
-              <v-img src="logo/or1.png" max-width="55" max-height="55"></v-img>
-            </v-btn>
-            <v-btn class="mx-6" v-for="nav_text in header_navigation_text" :key="nav_text" plain link>
-              {{ nav_text }}
-            </v-btn>
-          </v-card-text>
-        </v-row>
-      </v-card>
+    <!-- Navigation Bar -->
+    <v-card class="text-center mt-5 mb-5" flat tile width="100%">
+      <!-- Card for Icons -->
+      <v-row>
+        <v-card-text link>
+          <v-btn plain>
+            <v-img src="logo/or1.png" max-width="55" max-height="55"></v-img>
+          </v-btn>
+          <v-btn class="mx-6" v-for="nav_text in header_navigation_text" :key="nav_text" plain link>
+            {{ nav_text }}
+          </v-btn>
+        </v-card-text>
+      </v-row>
+    </v-card>
 
+    <v-main>
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
         <!-- Greetings -->
@@ -24,7 +24,7 @@
             <div class="text-h2 font-weight-black">
               ORLY PLAZA
               <div class="text-h6 font-weight-black">
-                IoT Specialist
+                OpenCV Engineer
               </div>
             </div>
           </v-card-text>
@@ -39,7 +39,7 @@
         </v-card>
 
         <!-- Introduction Text -->
-        <v-card class="text-left mx-auto pa-3" max-width="50%" flat tile>
+        <v-card class="text-left mx-auto pa-1" max-width="50%" flat tile>
           <v-row>
             <v-col>
               <p class="text-center mt-10 mb-10">
@@ -49,13 +49,29 @@
           </v-row>
         </v-card>
 
+        <!-- Things That I get in touch! -->
+        <v-card class="text-center" flat tile width="100%">
+
+          <!-- Card for Text -->
+          <v-card-text>
+            <p>{{ things }}</p>
+          </v-card-text>
+
+          <v-card-text icon v-for="tech in technology" :key="tech">
+            <v-row justify="center">
+              <v-icon dense>{{ tech }}</v-icon>
+            </v-row>
+          </v-card-text>
+        </v-card>
+
         <!-- If using vue-router -->
         <router-view></router-view>
       </v-container>
+
     </v-main>
 
     <!-- Router -->
-    <v-footer class="" app padless>
+    <v-footer class="" app dark padless>
       <v-card class="text-center" flat tile width="100%">
 
         <!-- Card for Icons -->
@@ -64,6 +80,9 @@
             <v-icon dense>{{ icon }}</v-icon>
           </v-btn>
         </v-card-text>
+
+        <v-divider></v-divider>
+
         <!-- Card for Text -->
         <v-card-text>
           <p>{{ Author }} | &copy;2022</p>
@@ -95,6 +114,15 @@ export default {
 
       Author: 'ENGR. PLAZA ORLY',
       Introduction_text: "I am a self-motivated Computer Engineer and an aspiring Python developer, and I use C++ for research and development. I'm fascinated by technology innovation. to get an opportunity that allows me to showcase my skills and contribute to the company's growth.",
+
+      things: "Technology that I get in touch",
+      technology: [
+        'mdi-language-python',
+        'mdi-git',
+        'mdi-hamburger',
+        'mdi-react',
+        'mdi-android-studio',
+      ]
     }
   }
 }
